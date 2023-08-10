7c6c88a081b33b6dd3e35f04bce9238a7f0c0e11
yuyinws <lyc657508366@gmail.com>
2023-08-10 17:34:11 +0800

    feat: dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/toggle-theme";
 
 export default function Home() {
   return (
     <div>
+      <ModeToggle></ModeToggle>
       <Button className="ml-5">Click Me</Button>
     </div>
   );
