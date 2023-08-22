bf5ad0c603e6166a97ee8b845a7b18abf6a35a24
yuyinws <lyc657508366@gmail.com>
2023-08-22 11:41:03 +0800

    fix: dynamic

--- app/api/gh/user/route.ts
+++ app/api/gh/user/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url)
